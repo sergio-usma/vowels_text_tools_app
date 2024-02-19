bfee30b641b8f3859ab3a762b4f19d4c5a48a6b2
Sergio Usma <sergioandresusma@hotmail.com>
2024-02-19 17:33:40 -0500

    Add eventListeners to inputs instead buttons

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 const vowelInput = document.getElementById('vowelInput');
 const vowelResult = document.getElementById('vowelResult');
-const vowelButton = document.getElementById('vowelButton');
 
 const wordInput = document.getElementById('wordInput');
 const wordResult = document.getElementById('wordResult');
-const wordButton = document.getElementById('wordButton');
 
 const characterInput = document.getElementById('characterInput');
 const characterResult = document.getElementById('characterResult');
-const characterButton = document.getElementById('characterButton');
 
 function isVowel(char) {
   const vowels = ['a', 'e', 'i', 'o', 'u'];
@@ -18,23 +15,23 @@ function isVowel(char) {
 function countVowels() {
   const input = vowelInput.value.toLowerCase();
   const vowels = input.split('').filter(isVowel);
-  vowelResult.innerHTML = `Result: ${vowels.length} vowels`;
+  vowelResult.innerHTML = `${vowels.length} vowels`;
   vowelResult.classList.remove('hidden');
 }
 
 function countWords() {
   const input = wordInput.value;
   const words = input.split(' ').filter((word) => word !== '');
-  wordResult.innerHTML = `Result: ${words.length} words`;
+  wordResult.innerHTML = `${words.length} words`;
   wordResult.classList.remove('hidden');
 }
 
 function countCharacters() {
   const input = characterInput.value;
-  characterResult.innerHTML = `Result: ${input.length} characters`;
+  characterResult.innerHTML = `${input.length} characters`;
   characterResult.classList.remove('hidden');
 }
 
-vowelButton.addEventListener('click', countVowels);
-wordButton.addEventListener('click', countWords);
-characterButton.addEventListener('click', countCharacters);
+vowelInput.addEventListener('input', countVowels);
+wordInput.addEventListener('input', countWords);
+characterInput.addEventListener('input', countCharacters);
